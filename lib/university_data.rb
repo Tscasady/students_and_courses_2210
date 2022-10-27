a2284cc2fed79d367ac54f0791d35b0606c01a2b
Thomas Casady <tscasady@gmail.com>
2022-10-27 11:03:37 -0500

    feat: Complete UniversityData.

--- lib/university_data.rb
+++ lib/university_data.rb
@@ -1,2 +1,25 @@
 class UniversityData
+
+  attr_reader :gradebooks
+
+  def initialize()
+    @gradebooks = []
+  end
+
+  def add_gradebook(gradebook)
+    @gradebooks << gradebook
+  end
+
+  def list_all_students
+    @gradebooks.map do |gradebook|
+      gradebook.list_all_students
+    end.flatten
+  end
+
+  def list_all_students_below_grade(grade)
+    @gradebooks.map do |gradebook|
+      gradebook.list_students_below_grade(grade)
+    end.flatten
+  end
+
 end

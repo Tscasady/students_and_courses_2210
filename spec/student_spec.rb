17df23c067c51579f941a7087ecffc71b2aa185b
Thomas Casady <tscasady@gmail.com>
2022-10-27 10:05:49 -0500

    test: Add tests for student class.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,27 @@
 require './lib/student'
 
 RSpec.describe Student do
+  describe '#initialize' do
+    it 'exists' do
+    end
+
+    it 'has a name' do
+    end
+
+    it 'has an age' do
+    end
+
+    it 'has an empty scores array' do
+    end
+  end
+
+  describe '#log_score' do
+    it 'can return a score' do
+    end
+  end
+
+  describe '#grade' do
+    it 'can return an average of scores' do
+    end
+  end
 end

d5ed326e348d6258e393486ad3ade39e22cde9f4
Thomas Casady <tscasady@gmail.com>
2022-10-27 10:10:38 -0500

    test: Complete empty student tests.

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -3,25 +3,47 @@ require './lib/student'
 RSpec.describe Student do
   describe '#initialize' do
     it 'exists' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student).to be_a Student
     end
 
     it 'has a name' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.name).to eq "Morgan"
     end
 
     it 'has an age' do
+      student = Student.new({name: "Morgan", age: 21})
+
+      expect(student.age).to eq 21
     end
 
     it 'has an empty scores array' do
+      student = Student.new({name: "Morgan", age: 21})
+      
+      expect(student.scores).to eq []
     end
   end
 
   describe '#log_score' do
-    it 'can return a score' do
+    it 'can add a score to the scores array' do
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_score(89)
+      student.log_score(78)
+
+      expect(student.scores).to eq [89, 78]
     end
   end
 
   describe '#grade' do
     it 'can return an average of scores' do
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_score(89)
+      student.log_score(78)
+
+      expect(student.grade).to eq 83.5
     end
   end
 end
